782a9589928a79370eb1631595400cc2b1ff8c77
capsnivelouro <0000999275@senaimgaluno.com.br>
2024-07-17 22:09:47 -0300

    footer

---
+++ includeFooter.js
@@ -0,0 +1,10 @@
+// includeHeader.js
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("footer.html")
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById("footer-container").innerHTML = data;
+      })
+      .catch(error => console.error("Erro ao carregar o cabeçalho:", error));
+  });
+  
